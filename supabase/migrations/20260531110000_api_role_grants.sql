c3b4aaf3cf94bda80c348989d4821a4c8014a972
radwa-talaat <radwatalaat.rt@gmail.com>
2026-05-31 11:03:10 +0300

    Update project after Codex fixes

---
+++ supabase/migrations/20260531110000_api_role_grants.sql
@@ -0,0 +1,32 @@
+grant usage on schema public to anon, authenticated, service_role;
+
+grant select on all tables in schema public to anon;
+grant all privileges on all tables in schema public to authenticated;
+grant all privileges on all tables in schema public to service_role;
+
+grant usage, select on all sequences in schema public to authenticated;
+grant all privileges on all sequences in schema public to service_role;
+
+grant execute on all functions in schema public to authenticated;
+grant execute on all functions in schema public to service_role;
+
+alter default privileges in schema public
+grant select on tables to anon;
+
+alter default privileges in schema public
+grant all privileges on tables to authenticated;
+
+alter default privileges in schema public
+grant all privileges on tables to service_role;
+
+alter default privileges in schema public
+grant usage, select on sequences to authenticated;
+
+alter default privileges in schema public
+grant all privileges on sequences to service_role;
+
+alter default privileges in schema public
+grant execute on functions to authenticated;
+
+alter default privileges in schema public
+grant execute on functions to service_role;
